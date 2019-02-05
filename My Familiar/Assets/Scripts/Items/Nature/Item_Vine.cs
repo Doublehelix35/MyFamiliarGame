03d2d1bbce2870ff8d219d99f4a5134372f54eba
Mitchell Dixon <mitchelldixon35@gmail.com>
2019-02-05 16:58:19 +0000

    Nature item completed and wind item started
    Completed nature item
    Wind item has been attempted but not working yet.

---
+++ My Familiar/Assets/Scripts/Items/Nature/Item_Vine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Vine : Item
+{
+    public GameObject HighestParentVine; // Highest vine in parent heirarchy
+
+    // Player calls this when vine collides with it
+    public override void Interact(GameObject player)
+    {
+        // Deal damage
+        player.GetComponent<Character>().ChangeHealth(-Damage);
+
+        // Give Happiness value
+        player.GetComponent<Character>().ChangeHappiness(HappinessChangeValue);
+
+        // Give spec points
+        player.GetComponent<Character>().GainElementSpecPoints(Elements.ElementType.Nature, SpecPointsGiven);
+
+        // Give exp
+        player.GetComponent<Character>().GainExp(ExpPointsGiven);
+
+        // Destroy whole vine
+        if(HighestParentVine.GetComponent<Item_Vine>().Uses <= 0)
+        {
+            Destroy(HighestParentVine);
+        }
+        else
+        {
+            HighestParentVine.GetComponent<Item_Vine>().Uses--; // Only one vine part track the uses
+        }
+        
+    }
+}
