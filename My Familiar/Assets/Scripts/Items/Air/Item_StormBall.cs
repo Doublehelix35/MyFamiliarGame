fedebca41860a8b1c6bca15a26dad30db161dc5b
Mitchell Dixon <mitchelldixon35@gmail.com>
2019-02-06 11:58:02 +0000

    Air training item finihsed

---
+++ My Familiar/Assets/Scripts/Items/Air/Item_StormBall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_StormBall : Item
+{
+    public float WindMagnitude = 100f;
+
+    // Player calls this when Storm Ball collides with it
+    public override void Interact(GameObject player)
+    {
+        // Push player away
+        player.GetComponent<Rigidbody>().AddExplosionForce(WindMagnitude, transform.position, 0f, 0.05f, ForceMode.Impulse);
+
+        // Deal damage
+        player.GetComponent<Character>().ChangeHealth(-Damage);
+
+        // Give Happiness value
+        player.GetComponent<Character>().ChangeHappiness(HappinessChangeValue);
+
+        // Give spec points
+        player.GetComponent<Character>().GainElementSpecPoints(Elements.ElementType.Air, SpecPointsGiven);
+
+        // Give exp
+        player.GetComponent<Character>().GainExp(ExpPointsGiven);
+
+        // Destroy self
+        if(Uses <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Uses--;
+        }
+        
+    }
+}
