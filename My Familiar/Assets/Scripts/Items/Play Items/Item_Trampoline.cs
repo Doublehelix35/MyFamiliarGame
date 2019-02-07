2d24df3da416978fce619160308dc87f4c2a57ae
Mitchell Dixon <mitchelldixon35@gmail.com>
2019-02-07 15:47:57 +0000

    Trampoline item + Material naming convention
    Trampoline play item in and working
    Material naming convention
    SLight changes to file structure

---
+++ My Familiar/Assets/Scripts/Items/Play Items/Item_Trampoline.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Trampoline : Item
+{
+    public float BounceMagnitude = 100f;
+
+    // Player calls this when Trampoline collides with it
+    public override void Interact(GameObject player)
+    {
+        // Bounce player up
+        player.GetComponent<Rigidbody>().AddExplosionForce(BounceMagnitude, transform.position, 0f, 1f, ForceMode.Impulse);
+
+        // Give Happiness value
+        player.GetComponent<Character>().ChangeHappiness(HappinessChangeValue);
+
+        // Destroy self
+        if(Uses <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Uses--;
+        }
+        
+    }
+}
