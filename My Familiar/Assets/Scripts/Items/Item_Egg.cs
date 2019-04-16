5969a06cf2e6e82fbb74e2a79f1af026ae8ee9ef
Mitchell Dixon <mitchelldixon35@gmail.com>
2019-04-16 16:15:29 +0100

    Egg progress
    Added egg sprite
    Adjusted facial feature prefabs
    Started egg script

---
+++ My Familiar/Assets/Scripts/Items/Item_Egg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Egg : Item
+{
+    // Egg that player hatches from when first created
+
+    // GM calls this when this is touched
+    public override void Interact(GameObject player)
+    {
+        // Change egg sprite
+
+        // Destroy self
+        if (Uses <= 0)
+        {
+            // Spawn player
+
+            // Destroy
+            Destroy(gameObject);
+        }
+        else
+        {
+            Uses--;
+        }
+    }
+}
