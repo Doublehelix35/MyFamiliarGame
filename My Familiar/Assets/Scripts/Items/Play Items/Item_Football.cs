f046be0623d2f87328bc3ef38dc747f2b938eba1
Mitchell Dixon <mitchelldixon35@gmail.com>
2019-07-19 16:07:19 +0100

    Apple 3d model + Football
    Apple improvements
    Football added
    Football script
    Folder structure improved

---
+++ My Familiar/Assets/Scripts/Items/Play Items/Item_Football.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Football : Item
+{
+    // Player calls this when Football collides with it
+    public override void Interact(GameObject player)
+    {
+        // Give Happiness value
+        player.GetComponent<Character>().ChangeHappiness(HappinessChangeValue);
+
+        // Notify observers
+        Notify(gameObject, Observer.Events.ItemUsed);
+
+        // Destroy self
+        if (Uses <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Uses--;
+        }
+
+    }
+}
